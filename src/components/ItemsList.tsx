ff22ad402231e1c9f1ea2ca73d1525cb2a407125
Othman Shareef <othmanosx@gmail.com>
2022-08-21 13:55:48 +0300

    Feat: add list arrow

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -1,12 +1,16 @@
 import { items } from "../data"
+import Arrow from "./Arrow"
 import Item from "./Item"
 
 export default function ItemsList() {
   return (
-    <div className="items-list">
-      {items.map((item) => (
-        <Item key={item.id} item={item} />
-      ))}
+    <div className="list-container">
+      <div className="items-list">
+        {items.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </div>
+      <Arrow />
     </div>
   )
 }
